342d9bd9ae62bd3047fa3dabdd25d489c9134464
trantrunghieuvku <trantrunghieua13@gmail.com>
2020-10-21 21:35:07 +0700

    .

---
+++ src/main/java/com/vku/qlktx/QlktxApplication.java
@@ -0,0 +1,13 @@
+package com.vku.qlktx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlktxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlktxApplication.class, args);
+	}
+
+}
