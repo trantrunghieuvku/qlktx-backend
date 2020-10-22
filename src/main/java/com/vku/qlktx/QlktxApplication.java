025d9daf89039df18d7a98dc5d17661353822560
Myhanh255 <nmhanh.18it1@sict.udn.vn>
2020-10-22 10:35:56 +0700

    app.pro

--- src/main/java/com/vku/qlktx/QlktxApplication.java
+++ src/main/java/com/vku/qlktx/QlktxApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QlktxApplication {
+public class QlktxApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(QlktxApplication.class, args);
